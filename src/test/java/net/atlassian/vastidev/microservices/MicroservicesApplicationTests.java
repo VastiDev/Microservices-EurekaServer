fb3b1414536e8857298959bf7e9a5689ed3cce29
VastiDev <vastiane1@gmail.com>
2023-12-15 22:48:43 -0300

    starts project

---
+++ src/test/java/net/atlassian/vastidev/microservices/MicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package net.atlassian.vastidev.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
